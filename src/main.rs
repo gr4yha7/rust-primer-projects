d208b14650df173672e784b50d1e3942c432130d
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2026-01-31 21:51:10 +0100

    chore: update main module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+#![allow(unused)]
 use std::{
     fs::File,
-    io::{BufRead, BufReader, Result}, path::PathBuf,
+    io::{BufRead, BufReader}, path::PathBuf,
 };
 use clap::Parser;
 use serde::{Serialize, Deserialize};
-use chrono::{DateTime};
+use anyhow::Result;
+use loggaliza::log_analyzer::{AnalyzerError, LogEntry, Logs};
 
 #[derive(Parser)]
 #[command(name="Loggaliza", version, about("Server logs file analyzer"), long_about = None)]
@@ -13,56 +15,51 @@ struct Opts {
     input_file: PathBuf,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(untagged)]
-enum ApiError {
-    DetailedError {
-        code: String,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        name: Option<String>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        #[serde(rename = "statusCode")]
-        status_code: Option<u16>,
-    },
-    SimpleError {
-        code: String,
-    },
-    SimpleErrorString(String),
-}
+// #[derive(Debug, Serialize, Deserialize)]
+// #[serde(untagged)]
+// enum ApiError {
+//     DetailedError {
+//         code: String,
+//         #[serde(skip_serializing_if = "Option::is_none")]
+//         name: Option<String>,
+//         #[serde(skip_serializing_if = "Option::is_none")]
+//         #[serde(rename = "statusCode")]
+//         status_code: Option<u16>,
+//     },
+//     SimpleError {
+//         code: String,
+//     },
+//     SimpleErrorString(String),
+// }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct LogEntry {
-    error: ApiError,
-    level: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    method: Option<String>,
-    message: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    path: Option<String>,
-    service: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    timestamp: Option<String>,
-}
+// #[derive(Debug, Serialize, Deserialize)]
+// struct LogEntry {
+//     error: ApiError,
+//     level: String,
+//     #[serde(skip_serializing_if = "Option::is_none")]
+//     method: Option<String>,
+//     message: String,
+//     #[serde(skip_serializing_if = "Option::is_none")]
+//     path: Option<String>,
+//     service: String,
+//     #[serde(skip_serializing_if = "Option::is_none")]
+//     timestamp: Option<String>,
+// }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), AnalyzerError> {
     let args = Opts::parse();
 
     let file_exists = args.input_file.try_exists()?;
     if !file_exists {
         panic!("File does not exist")
     }
-    // let file_path: PathBuf = PathBuf::from(args.input_file);
-    let file = File::open(args.input_file)?;
-    let reader = BufReader::new(file);
+    let mut logs: Logs = Logs::default();
+    logs.read_and_parse_log(args.input_file)?;
 
-    for line in reader.lines() {
-        // println!("read line: {:?}", line);
-        let entry = line.unwrap();
-        let log_entry: LogEntry = serde_json::from_str(&entry)?;
-        println!("log entry: {:?}", log_entry);
-    }
-
+    // let logs_by_level: Vec<&LogEntry> = logs.filter_by_level("INFO")?.collect();
+    // let logs_by_endpoint: Vec<&LogEntry> = logs.filter_by_endpoint("api/products")?.collect();
+    let logs_by_date_range: Vec<&LogEntry> = logs.filter_by_date_range("2024-01-16", "2024-01-18")?.collect();
+    // print!("logs by level: {:?}", logs_by_level);
+    print!("logs by date_range: {:?}", logs_by_date_range);
     Ok(())
-
-    // println!("input file path: {:?}", args.input_file);
 }
